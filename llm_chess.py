14df80897d10315ee66c9e246fbd6ce2905f17f1
Maxim Saplin <smaxmail@gmail.com>
2024-10-11 17:00:42 +0300

    Param comments

--- llm_chess.py
+++ llm_chess.py
@@ -36,9 +36,9 @@ white_player_type = PlayerType.RANDOM_PLAYER
 black_player_type = PlayerType.LLM_BLACK
 use_fen_board = True  # Whther to use graphical UNICODE representation board OR single line FEN format (returned from get_current_board)
 max_game_moves = 200  # maximum number of game moves before terminating
-max_llm_turns = 10  # how many turns can an LLM make while making a move
-max_failed_attempts = 3  # number of wrong replies/actions before halting the game and giving the player a loss
-throttle_delay_moves = 0  # some LLM provider might thorttle frequent API reuqests, make a delay (in seconds) between moves
+max_llm_turns = 10  # how many conversation turns can an LLM make while making a move
+max_failed_attempts = 3  # number of wrong replies/actions (e.g. picking non existing action) before stopping the game and giving a loss
+throttle_delay_moves = 1  # some LLM providers might thorttle frequent API reuqests, make a delay (in seconds) between moves
 visualize_board = True  # You can skip board visualization to speed up execution
 
 
