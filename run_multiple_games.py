d79015ad40e45734b23a02361a72ac4cb1086549
Maxim Saplin <smaxmail@gmail.com>
2024-10-12 10:35:57 +0300

    gemini-1.5-flash-001

--- run_multiple_games.py
+++ run_multiple_games.py
@@ -5,7 +5,7 @@ from llm_chess import run
 
 # Parameters
 NUM_REPETITIONS = 10  # Set the number of games to run
-LOG_FOLDER = "_logs/_12.10.2024_gpt-4o-2024-08-06"  # Set the folder to store logs
+LOG_FOLDER = "_logs/_12.10.2024_gemini-1.5-flash-001"  # Set the folder to store logs
 STORE_INDIVIDUAL_LOGS = True
 
 # ALSO CHECK INDIVIDUAL PARAMS AT `llm_chess.py`
