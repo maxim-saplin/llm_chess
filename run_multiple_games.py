e7ba0b7fd3ab538b5d26a63d741d41ff49143fe6
Maxim Saplin <smaxmail@gmail.com>
2024-11-08 13:53:18 +0300

    More models

--- run_multiple_games.py
+++ run_multiple_games.py
@@ -5,7 +5,7 @@ from llm_chess import run
 
 # Parameters
 NUM_REPETITIONS = 10  # Set the number of games to run
-LOG_FOLDER = "_logs/no_reflection/_07.11.2024_llama-3-70b-instruct-awq"  # Set the folder to store logs
+LOG_FOLDER = "_logs/no_reflection/_08.11.2024_anthropic.claude-v3-5-sonnet"  # Set the folder to store logs
 
 STORE_INDIVIDUAL_LOGS = True
 
