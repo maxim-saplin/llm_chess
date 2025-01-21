c4a42fee37bdbffc7ad94de6649edd129d053242
Maxim Saplin <smaxmail@gmail.com>
2025-01-21 23:26:39 +0300

    Web update

--- data_processing/get_refined_csv.py
+++ data_processing/get_refined_csv.py
@@ -1,5 +1,5 @@
 import os
-from aggregate_logs_to_csv import aggregate_models_to_csv
+from aggregate_logs_to_csv import aggregate_models_to_csv, MODEL_OVERRIDES
 from aggr_to_refined import convert_aggregate_to_refined
 
 LOGS_DIR = "_logs/no_reflection"
@@ -9,7 +9,7 @@ REFINED_CSV = "data_processing/refined.csv"
 
 def main():
     # Step 1: Aggregate logs to CSV
-    aggregate_models_to_csv(LOGS_DIR, AGGREGATE_CSV)
+    aggregate_models_to_csv(LOGS_DIR, AGGREGATE_CSV, MODEL_OVERRIDES)
 
     # Step 2: Convert aggregated CSV to refined CSV
     convert_aggregate_to_refined(AGGREGATE_CSV, REFINED_CSV)
