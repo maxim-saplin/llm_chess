9f71d9bcea426e1a8f721147ffc996f0b4a29ec7
Maxim Saplin <smaxmail>
2025-04-03 13:31:42 +0300

    More models

--- run_multiple_games.py
+++ run_multiple_games.py
@@ -12,12 +12,12 @@ model_name = black_config["config_list"][0]["model"]
 
 # Parameters
 NUM_REPETITIONS = 67  # Set the number of games to run
-LOG_FOLDER = f"_logs/new/{model_name}-PGN/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
+LOG_FOLDER = f"_logs/new/{model_name}/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
 STORE_INDIVIDUAL_LOGS = True
 
-llm_chess.board_representation_mode = llm_chess.BoardRepresentation.UNICODE_WITH_PGN
-llm_chess.throttle_delay = 0
-llm_chess.dialog_turn_delay = 0
+# llm_chess.board_representation_mode = llm_chess.BoardRepresentation.UNICODE_WITH_PGN
+llm_chess.throttle_delay = 1
+llm_chess.dialog_turn_delay = 1
 
 # ALSO CHECK INDIVIDUAL PARAMS AT `llm_chess.py`
 # Hyper params such as temperature are defined in `utils.py`
