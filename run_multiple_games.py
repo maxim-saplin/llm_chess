4c016f244701589f94a154bb8b954ec91deefe15
Maxim Saplin <smaxmail>
2025-04-09 09:46:54 +0300

    Minor changes

--- run_multiple_games.py
+++ run_multiple_games.py
@@ -11,19 +11,19 @@ _, black_config = get_llms_autogen()
 model_name = black_config["config_list"][0]["model"]
 
 # Parameters
-NUM_REPETITIONS = 30  # Set the number of games to run
-# LOG_FOLDER = f"_logs/new/{model_name}/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
-LOG_FOLDER = f"_logs/llm_vs_llm/4o_mini_vs_flash_2_PGN/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
+NUM_REPETITIONS = 42  # Set the number of games to run
+LOG_FOLDER = f"_logs/new/{model_name}/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
+# LOG_FOLDER = f"_logs/llm_vs_llm/flash_2_vs_4o_mini/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
 STORE_INDIVIDUAL_LOGS = True
 
 # ALSO CHECK INDIVIDUAL PARAMS AT `llm_chess.py`
 # Hyper params such as temperature are defined in `utils.py`
 
-llm_chess.board_representation_mode = llm_chess.BoardRepresentation.UNICODE_WITH_PGN
+# llm_chess.board_representation_mode = llm_chess.BoardRepresentation.UNICODE_WITH_PGN
 llm_chess.throttle_delay = 1
 llm_chess.dialog_turn_delay = 1
 
-llm_chess.white_player_type = llm_chess.PlayerType.LLM_WHITE
+# llm_chess.white_player_type = llm_chess.PlayerType.LLM_WHITE
 
 
 def run_games(num_repetitions, log_folder=LOG_FOLDER):
