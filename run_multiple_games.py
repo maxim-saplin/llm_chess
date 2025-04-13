982b502b60f8c1787d96a977a4772da5e25ab546
Maxim Saplin <smaxmail@gmail.com>
2025-04-13 17:25:47 +0300

    GPT-4o MoA

--- run_multiple_games.py
+++ run_multiple_games.py
@@ -12,14 +12,14 @@ model_name = black_config["config_list"][0]["model"]
 
 # Parameters
 NUM_REPETITIONS = 30  # Set the number of games to run
-# LOG_FOLDER = f"_logs/new/{model_name}/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
-LOG_FOLDER = f"_logs/llm_vs_llm/haiku_35_vs_4o_mini/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
+LOG_FOLDER = f"_logs/new/moa_{model_name}_t00_07_03/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
+# LOG_FOLDER = f"_logs/llm_vs_llm/haiku_35_vs_4o_mini/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
 STORE_INDIVIDUAL_LOGS = True
 
 # ALSO CHECK INDIVIDUAL PARAMS AT `llm_chess.py`
 # Hyper params such as temperature are defined in `utils.py`
 
-llm_chess.throttle_delay = 1
+llm_chess.throttle_delay = 0
 llm_chess.dialog_turn_delay = 1
 
 # llm_chess.white_player_type = llm_chess.PlayerType.LLM_WHITE
