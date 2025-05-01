a23579bc9685b8aad608aeeebfe343fa7edc5ada
Maxim Saplin <smaxmail>
2025-05-01 23:31:47 +0300

    Run multiple defaults

--- run_multiple_games.py
+++ run_multiple_games.py
@@ -8,17 +8,17 @@ import llm_chess
 
 model_name = llm_chess.llm_config_black["config_list"][0]["model"]
 
-NUM_REPETITIONS = 42  # Set the number of games to run
+NUM_REPETITIONS = 33  # Set the number of games to run
 LOG_FOLDER = f"_logs/new/{model_name}/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
 # LOG_FOLDER = f"_logs/llm_vs_llm/haiku_35_vs_4o_mini/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
-# LOG_FOLDER = f"_logs/sotckfish_vs_llm/{model_name}-low_pgn/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
+# LOG_FOLDER = f"_logs/dragon_vs_llm/lvl-1_vs_{model_name}-low/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
 # LOG_FOLDER = f"_logs/ensemble-ai/41-mini_t00_t03_t05_t07_t10_t10_t03/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
 STORE_INDIVIDUAL_LOGS = True
 
 ## ALSO CHECK INDIVIDUAL PARAMS AT `llm_chess.py`, hyper params defaults are defined in `utils.py`
 
 llm_chess.throttle_delay = 0
-llm_chess.dialog_turn_delay = 0
+llm_chess.dialog_turn_delay = 1
 
 # llm_chess.temp_override = "remove" # For OpenAI and Anthropic thinking modes
 # llm_chess.reasoning_effort = "low" # Default is None, used with OpenAI models low, medium, or high
@@ -29,7 +29,9 @@ llm_chess.dialog_turn_delay = 0
 ## r"<reasoning>.*?</reasoning>" - Reka Flash
 # llm_chess.remove_text = None
 
-# llm_chess.white_player_type = llm_chess.PlayerType.CHESS_ENGINE_STOCKFISH
+# llm_chess.dragon_path = "dragon/dragon-linux"
+
+# llm_chess.white_player_type = llm_chess.PlayerType.CHESS_ENGINE_DRAGON
 # llm_chess.black_player_type = llm_chess.PlayerType.LLM_NON
 # llm_chess.board_representation_mode = llm_chess.BoardRepresentation.UNICODE_WITH_PGN
 # llm_chess.rotate_board_for_white = True
@@ -90,7 +92,7 @@ def run_games(num_repetitions, log_folder=LOG_FOLDER):
     for _ in range(num_repetitions):
         # Call the run function and get the game stats
         game_stats, player_white, player_black = run(
-            log_dir=log_folder
+            log_dir=log_folder if STORE_INDIVIDUAL_LOGS else None
         )
 
         moves_list.append(game_stats["number_of_moves"])  # Track moves
