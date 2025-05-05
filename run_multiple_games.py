afbbab55a121a04b2b408cde4b2a12ec49f94899
Maxim Saplin <smaxmail>
2025-05-05 17:00:56 +0300

    More llm vs XYZ

--- run_multiple_games.py
+++ run_multiple_games.py
@@ -9,9 +9,9 @@ import llm_chess
 model_name = llm_chess.llm_config_black["config_list"][0]["model"]
 
 NUM_REPETITIONS = 33  # Set the number of games to run
-LOG_FOLDER = f"_logs/new/{model_name}/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
+# LOG_FOLDER = f"_logs/new/{model_name}/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
 # LOG_FOLDER = f"_logs/llm_vs_llm/haiku_35_vs_4o_mini/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
-# LOG_FOLDER = f"_logs/dragon_vs_llm/lvl-1_vs_{model_name}-low/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
+LOG_FOLDER = f"_logs/dragon_vs_llm/lvl-2_vs_{model_name}-high-timeout-20m/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
 # LOG_FOLDER = f"_logs/ensemble-ai/41-mini_t00_t03_t05_t07_t10_t10_t03/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
 STORE_INDIVIDUAL_LOGS = True
 
@@ -20,8 +20,8 @@ STORE_INDIVIDUAL_LOGS = True
 llm_chess.throttle_delay = 0
 llm_chess.dialog_turn_delay = 1
 
-# llm_chess.temp_override = "remove" # For OpenAI and Anthropic thinking modes
-# llm_chess.reasoning_effort = "low" # Default is None, used with OpenAI models low, medium, or high
+llm_chess.temp_override = "remove" # For OpenAI and Anthropic thinking modes
+llm_chess.reasoning_effort = "high" # Default is None, used with OpenAI models low, medium, or high
 # llm_chess.thinking_budget = 10000
 
 ## r"<think>.*?</think>" - Deepseek R1 Distil, Phi-4
@@ -29,9 +29,10 @@ llm_chess.dialog_turn_delay = 1
 ## r"<reasoning>.*?</reasoning>" - Reka Flash
 # llm_chess.remove_text = None
 
-# llm_chess.dragon_path = "dragon/dragon-linux"
+llm_chess.dragon_path = "dragon/dragon-linux"
+llm_chess.dragon_level = 2
 
-# llm_chess.white_player_type = llm_chess.PlayerType.CHESS_ENGINE_DRAGON
+llm_chess.white_player_type = llm_chess.PlayerType.CHESS_ENGINE_DRAGON
 # llm_chess.black_player_type = llm_chess.PlayerType.LLM_NON
 # llm_chess.board_representation_mode = llm_chess.BoardRepresentation.UNICODE_WITH_PGN
 # llm_chess.rotate_board_for_white = True
