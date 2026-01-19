244bbf6359b229c3c1616dabc0078e4033f7cbb4
Maxim Saplin <smaxmail@gmail.com>
2026-01-19 16:01:59 +0300

    LB

--- data_processing/get_refined_csv.py
+++ data_processing/get_refined_csv.py
@@ -144,7 +144,7 @@ FILTER_OUT_MODELS = [
     "rekaai_reka-flash-3@q6_k_l",
     "mixtral-8x7b-32768",
     "qwen2.5-vl-72b-instruct",
-    "gpt-5.1-codex-mini-2025-11-13-high",  ## TBD, to few runs
+    # "gpt-5.1-codex-mini-2025-11-13-high",  ## TBD, to few runs
     "gpt-5.1-codex-mini-2025-11-13-medium",  ## TBD, to few runs
     "chess-4b-thinking-1218",  ## RL experiment, to be ignored
     "ignore",  # models marked to be ignored via MODEL_OVERRIDES
